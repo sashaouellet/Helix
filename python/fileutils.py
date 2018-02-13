a75bd9bd633be1d2a213761ce4de7ec813a59894
Sasha Ouellet <souell20@localhost.localdomain>
2018-02-12 22:37:59 -0500

    Changed formatting of sequence and shot directories

--- python/fileutils.py
+++ python/fileutils.py
@@ -1,8 +1,8 @@
 import os
 
 PADDING = 4 # TODO pull from config file
-SEQUENCE_FORMAT = 's{}'
-SHOT_FORMAT = '{}'
+SEQUENCE_FORMAT = 'sq{}'
+SHOT_FORMAT = 's{}'
 
 def formatShotDir(seqNum, shotNum):
 	return os.path.join(SEQUENCE_FORMAT.format(str(seqNum).zfill(PADDING)), SHOT_FORMAT.format(str(shotNum).zfill(PADDING)))
