35f7355213d41c4bb4c40af8dff37ddaee8c5f68
Sasha Ouellet <souell20@mt-r223-12.ad.scad.edu>
2018-03-13 12:12:34 -0400

    Added basic visualization tool for the database schema

--- python/helix/manager/viewer.py
+++ python/helix/manager/viewer.py
@@ -1,10 +1,127 @@
+import helix
 from helix.database.database import *
 import helix.environment.environment as env
 
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4 import uic
+
+import os
+from collections import OrderedDict
+from datetime import datetime
+
+class VisualizerWindow(QMainWindow):
+	HEADER_MAPPINGS = OrderedDict([
+		('Name', 'name'),
+		('Author', 'author'),
+		('Creation', 'creation'),
+		('Version', 'version'),
+		('Published Version', 'pubVersion'),
+		('Extension', 'ext'),
+		('Sequence', 'seq')
+	])
+
+	def __init__(self):
+		super(VisualizerWindow, self).__init__()
+		uic.loadUi(os.path.join(helix.root, 'ui', 'visualizer.ui'), self)
+
+		self.model = QStandardItemModel()
+		self.model.setHorizontalHeaderLabels(list(VisualizerWindow.HEADER_MAPPINGS.keys()))
+		self.VIEW_tree.setModel(self.model)
+
+		self.makeConnections()
+		self.show()
+
+	def makeConnections(self):
+		self.ACT_openDB.triggered.connect(self.handleOpenDB)
+		self.ACT_reload.triggered.connect(self.handleDBReload)
+
+	def handleOpenDB(self):
+		test = '/home/souell20/mount/collaborative/560testing/.db/db.json'
+		self.dbLoc = QFileDialog.getOpenFileName(self, caption='Open Database File', directory=test, filter='Database Files (*.json)')
+
+		if os.path.exists(self.dbLoc):
+			self.db = Database(str(self.dbLoc))
+
+			self.populate()
+
+	def handleDBReload(self):
+		del self.db
+		self.model.clear()
+		self.model.setHorizontalHeaderLabels(list(VisualizerWindow.HEADER_MAPPINGS.keys()))
+
+		self.db = Database(str(self.dbLoc))
+
+		self.populate()
+
+	def populate(self):
+		self.ACT_reload.setEnabled(False)
+
+		for show in self.db.getShows():
+			self.addShow(show)
+
+		self.ACT_reload.setEnabled(True)
+
+		for col in range(self.VIEW_tree.header().count()):
+			self.VIEW_tree.resizeColumnToContents(col)
+
+	def addShow(self, show):
+		showItem = QStandardItem(show.get('name', 'undefined'))
+
+		for sequence in show.getSequences():
+			showItem.appendRow(self.getSequence(sequence))
+
+		self.addElements(showItem, show.getElements())
+		self.model.appendRow(showItem)
+
+	def getSequence(self, sequence):
+		sequenceItem = QStandardItem('Sequence {}'.format(sequence.get('num', -1)))
+
+		for shot in sequence.getShots():
+			sequenceItem.appendRow(self.getShot(shot))
+
+		self.addElements(sequenceItem, sequence.getElements())
+
+		return sequenceItem
+
+	def getShot(self, shot):
+		shotItem = QStandardItem('Shot {}'.format(shot.get('num', -1)))
+
+		self.addElements(shotItem, shot.getElements())
+
+		return shotItem
+
+	def addElements(self, parent, elements):
+		els = {}
+
+		for element in elements:
+			elItem = self.getElement(element)
+			elType = element.get('_DBOType', 'Element')
+			elTypeItem = els.get(elType, QStandardItem(elType))
+
+			elTypeItem.appendRow(elItem)
+
+			els[elType] = elTypeItem
+
+		for elTypeItem in els.itervalues():
+			parent.appendRow(elTypeItem)
+
+	def getElement(self, element):
+		ret = []
+
+		for val in VisualizerWindow.HEADER_MAPPINGS.itervalues():
+			data = str(element.get(val, 'undefined'))
+
+			if val == 'creation':
+				date = datetime.strptime(data, env.DATE_FORMAT)
+				data = date.strftime('%b %d %Y %I:%M%p')
+
+			ret.append(QStandardItem(data))
+
+		return ret
+
 if __name__ == '__main__':
-	dbLoc = env.getEnvironment('db')
+	app = QApplication(sys.argv)
+	window = VisualizerWindow()
 
-	if not dbLoc:
-		raise KeyError('Database location not set in your environment')
-
-	db = Database(dbLoc)
+	sys.exit(app.exec_())
