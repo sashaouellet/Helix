0c95712ab65954964dbbcbb3f654c2cbf18f3858
sashaouellet <spaouellet@me.com>
2018-08-04 16:32:21 -0700

    Updated to use qdarkstyle as submodule

--- python/helix/__init__.py
+++ python/helix/__init__.py
@@ -1,3 +1,7 @@
-import os
+import os, sys
 
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+stylesheetPath = os.path.join(root, 'python', 'QDarkStyleSheet')
+
+if stylesheetPath not in sys.path:
+	sys.path.insert(0, stylesheetPath)
